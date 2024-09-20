3b16f6597f97136da2448b1749511f37591b1306
Midverthub <ajayifiyin42@gmail.com>
2024-09-20 06:07:38 +0100

    changes made

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
 import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+const inter = Inter({ subsets: ["latin"] });
+const publicSans = Public_Sans({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${publicSans.className} `}>
+        {/* <body className={`${geistSans.variable} ${geistMono.variable} ${inter.className} `}> */}
         {children}
       </body>
-    </html>
+    </html >
   );
 }
